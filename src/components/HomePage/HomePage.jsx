492d5933e7ec3695d6167c0852fd375df7fc5a28
Mohamed Hamada <mohamed43628@gmail.com>
2022-09-23 10:42:02 +0200

    added the main section for the home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
-
+import Welcom from "./WelcomSection/Welcom";
+import "./HomePage.css";
 export default class HomePage extends Component {
     render() {
-        return <div>HomePage</div>;
+        return (
+            <div className="homePage">
+                <div className="container">
+                    <Welcom />
+                </div>
+            </div>
+        );
     }
 }
--- src/components/HomePage/WelcomSection/Welcom.jsx
+++ src/components/HomePage/WelcomSection/Welcom.jsx
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
-
+import "./Welcome.style.css";
+import axe from "../../../assets/axe.png";
 export default class Welcom extends Component {
     render() {
-        return <div>Welcom</div>;
+        return (
+            <div className="welcome">
+                <div className="content">
+                    <div className="hello">
+                        <span className="He">He</span>
+                        <span className="llo">llow</span>
+                    </div>
+                    <img src={axe} alt="" />
+                </div>
+            </div>
+        );
     }
 }
