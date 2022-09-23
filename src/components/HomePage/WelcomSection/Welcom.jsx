ad42f50f6e1e33e282e21576e9ef603599f63e38
Mohamed Hamada <mohamed43628@gmail.com>
2022-09-23 08:08:58 +0200

    added the intial nav items + the intial components for hoepage

---
+++ src/components/HomePage/WelcomSection/Welcom.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Welcom extends Component {
+    render() {
+        return <div>Welcom</div>;
+    }
+}
--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -6,20 +6,34 @@ import logoName from "../../assets/axes digital agency.png";
 export default class Nav extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="navbar">
+            <div className="navbar">
+                <div className="container">
                     <div className="brand">
                         <img src={logo} alt="" />
                         <img src={logoName} alt="" />
                     </div>
                     <div className="navLinks">
-                        <Link to="/Home">Home</Link>
-                        <Link to="/Work">Work</Link>
-                        <Link to="/Clients">Clients</Link>
-                        <Link to="/Services">Services</Link>
-                        <Link to="/Contact-us">Contact Us</Link>
-                        <Link to="/About-us">About Us</Link>
-                        <Link to="/Ourteam">Our team</Link>
+                        <Link className="navItem" to="/Home">
+                            Home
+                        </Link>
+                        <Link className="navItem" to="/Work">
+                            Work
+                        </Link>
+                        <Link className="navItem" to="/Clients">
+                            Clients
+                        </Link>
+                        <Link className="navItem" to="/Services">
+                            Services
+                        </Link>
+                        <Link className="navItem" to="/Contact-us">
+                            Contact Us
+                        </Link>
+                        <Link className="navItem" to="/About-us">
+                            About Us
+                        </Link>
+                        <Link className="navItem" to="/Ourteam">
+                            Our team
+                        </Link>
                     </div>
                 </div>
             </div>
