596d85a761286cd69e0e13c62274d9c5adb9fef5
Mohamed Hamada <mohamed43628@gmail.com>
2022-09-28 07:19:25 +0200

    services section

---
+++ src/components/HomePage/HelpYou/HelpYou.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+import "./HelpYou.css";
+import ServiceList from "./ServiceList";
+
+export default class HelpYou extends Component {
+    render() {
+        return (
+            <div className="HelpYou">
+                <div className="sectionTitle">
+                    <div className="rect"></div>
+                    <div className="title">
+                        <h1>We can Help you alot</h1>
+                        <h1>with your ...</h1>
+                    </div>
+                </div>
+                <ServiceList />
+            </div>
+        );
+    }
+}
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import Welcom from "./WelcomSection/Welcom";
 import "./HomePage.css";
+import HelpYou from "./HelpYou/HelpYou";
 export default class HomePage extends Component {
     render() {
         return (
             <div className="homePage">
                 <div className="container">
                     <Welcom />
+                </div>
+                <div>
+                    <HelpYou />
                 </div>
             </div>
         );
