c4ac544c8c25ad11817374461232067828ae62e1
aya880 <yoyamustafa22@gmail>
2022-10-18 23:48:49 +0200

    NavbarEdit

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Nav.style.css";
-import { FiMenu, FiX } from "react-icons/fi";
+import { FiMenu, FiX, FiChevronDown, FiChevronUp } from "react-icons/fi";
 import logo from "../../assets/logo1.png";
 import logoName from "../../assets/axes digital agency.png";
 import LoGo from "../../assets/LogO.png";
@@ -10,51 +10,104 @@ const Nav = () => {
   const toggleMenuClick = () => {
     setMenuClicked(!menulicked);
   };
+  const [dropclicked, setDropClicked] = useState(false);
+  const toggleDropClick = () => {
+    setDropClicked(!dropclicked);
+  };
   return (
     <div className="navbar">
-      <div className="brand">
-        <img src={LoGo} alt="" />
-      </div>
-      {menulicked ? (
-        <FiMenu
-          size={25}
-          className="navbarMenu__Icon"
-          onClick={toggleMenuClick}
-        />
-      ) : (
-        <FiX size={25} className="navbarMenu__Icon" onClick={toggleMenuClick} />
-      )}
-      <div
-        className={
-          menulicked ? "navbar__list" : "navbar__list navbar__list--active"
-        }
-      >
-        <div className="LogoList">
-          {" "}
-          <img src={logo} alt="" />
+      <div className="container">
+        <div className="brand">
+          <img src={LoGo} alt="" />
         </div>
+        {menulicked ? (
+          <FiMenu
+            size={25}
+            className="navbarMenu__Icon"
+            onClick={toggleMenuClick}
+          />
+        ) : (
+          <FiX
+            size={25}
+            className="navbarMenu__Icon"
+            onClick={toggleMenuClick}
+          />
+        )}
 
-        <Link className="navItem" to="/Home">
-          Home
-        </Link>
-        <Link className="navItem" to="/Work">
-          Work
-        </Link>
-        <Link className="navItem" to="/Clients">
-          Clients
-        </Link>
-        <Link className="navItem" to="/Services">
-          Services
-        </Link>
-        <Link className="navItem" to="/Ourteam">
-          Our team
-        </Link>
-        <Link className="navItem" to="/#">
-          Contact Us
-        </Link>
-        <Link className="navItem lastOne" to="/#">
-          About Us
-        </Link>
+        {dropclicked ? (
+          <FiChevronUp
+            size={25}
+            className="navbarDrop__Icon"
+            onClick={toggleDropClick}
+          />
+        ) : (
+          <FiChevronDown
+            size={25}
+            className="navbarDrop__Icon"
+            onClick={toggleDropClick}
+          />
+        )}
+        <div
+          className={
+            menulicked ? "navbar__list" : "navbar__list navbar__list--active"
+          }
+        >
+          <div className="LogoList">
+            {" "}
+            <img src={logo} alt="" />
+          </div>
+
+          <Link className="navItem" to="/Home">
+            Home
+          </Link>
+          <Link className="navItem" to="/Work">
+            Work
+          </Link>
+          <Link className="navItem" to="/Clients">
+            Clients
+          </Link>
+          <Link className="navItem" to="/Services">
+            Services
+          </Link>
+          <Link className="navItem" to="/Ourteam">
+            Our team
+          </Link>
+          <Link className="navItem" to="/#">
+            Contact Us
+          </Link>
+          <Link className="navItem lastOne" to="/#">
+            About Us
+          </Link>
+        </div>
+      </div>
+      <div className="container2">
+        <div
+          className={
+            dropclicked ? "navbar__drop" : "navbar__drop navbar__dropNone"
+          }
+        >
+          <Link className="navItem" to="/Home">
+            Home
+          </Link>
+          <Link className="navItem" to="/Work">
+            Work
+          </Link>
+          <Link className="navItem" to="/Clients">
+            Clients
+          </Link>
+          <Link className="navItem" to="/Services">
+            Services
+          </Link>
+          <Link className="navItem" to="/Ourteam">
+            Our team
+          </Link>
+          <Link className="navItem" to="/#">
+            Contact Us
+          </Link>
+          <Link className="navItem lastOne" to="/#">
+            About Us
+          </Link>
+        </div>
       </div>
     </div>
   );
