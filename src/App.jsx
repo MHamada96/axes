b79e7a9f80b7671ef0d9e4dc5bb2e1b585aa0061
aya880 <yoyamustafa22@gmail>
2022-10-30 21:09:11 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,25 @@ import HomePage from "./pages/HomePage/HomePage";
 import ServicesPage from "./pages/SercvicesPage/ServicesPage";
 import TeamPage from "./pages/TeamPage/TeamPage";
 import WorkPage from "./pages/WorkPage/WorkPage";
+import ServicesPageN from "./pages/ServicesPage_New/ServicesPageN";
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<HomePage />}></Route>
-                        <Route path="/Home" element={<HomePage />}></Route>
-                        <Route path="/Work" element={<WorkPage />}></Route>
-                        <Route
-                            path="/Clients"
-                            element={<ClientsPage />}
-                        ></Route>
-                        <Route
-                            path="/Services"
-                            element={<ServicesPage />}
-                        ></Route>
-                        <Route path="/Ourteam" element={<TeamPage />}></Route>
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/Home" element={<HomePage />}></Route>
+            <Route path="/Work" element={<WorkPage />}></Route>
+            <Route path="/Clients" element={<ClientsPage />}></Route>
+            <Route path="/Services" element={<ServicesPage />}></Route>
+            <Route path="/ServicesN" element={<ServicesPageN />}></Route>
+            <Route path="/Ourteam" element={<TeamPage />}></Route>
 
-                        <Route path="*" element={<h1>404 PAGE</h1>}></Route>
-                    </Routes>
-                </BrowserRouter>
-            </div>
-        );
-    }
+            <Route path="*" element={<h1>404 PAGE</h1>}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
--- src/components/HeadSection/HeadSection.jsx
+++ src/components/HeadSection/HeadSection.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import "./HeadSection.css";
 export default class HeadSection extends Component {
-    render() {
-        const { word1, word2, image } = this.props;
-        return (
-            <div className="Head container">
-                <div className="content">
-                    <div className="hello">
-                        <span className="Word1">{word1}</span>
-                        <span className="Word2">{word2}</span>
-                    </div>
-                    <img src={image} alt="" />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { word1, word2, image } = this.props;
+    return (
+      <div className="Head container">
+        <div className="content">
+          <div className="hello">
+            <span className="Word1">{word1}</span>
+            <span className="Word2">{word2}</span>
+          </div>
+          <img src={image} alt="" />
+        </div>
+      </div>
+    );
+  }
 }
