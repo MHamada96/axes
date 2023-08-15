d959c1c03f1a02917ecdb7195c44252d23e61c20
MHamada96 <mohamed43628@gmail.com>
2023-08-15 13:39:02 +0300

    start to update projects section

--- src/components/ProjectComponent/ProjectComponent.jsx
+++ src/components/ProjectComponent/ProjectComponent.jsx
@@ -1,15 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./ProjectComponent.css";
-export default class ProjectComponent extends Component {
-    render() {
-        return (
-            <div className="ProjectComponent container">
-                <span className="title">Jo Lion Project</span>
-                <Link to="#" className="link">
-                    Social Media Project
-                </Link>
-            </div>
-        );
-    }
+
+function ProjectComponent(props) {
+    const { title, category, imgURL } = props;
+    console.log(imgURL);
+    return (
+        <div
+            className="ProjectComponent container"
+            style={{
+                backgroundImage: `url(${imgURL})`,
+            }}
+        >
+            <span className="title">{title}</span>
+            <Link to="#" className="link">
+                {category}
+            </Link>
+        </div>
+    );
 }
+
+export default ProjectComponent;
--- src/components/ProjectsSection/ProjectList.jsx
+++ src/components/ProjectsSection/ProjectList.jsx
@@ -1,18 +1,53 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import ProjectComponent from "../ProjectComponent/ProjectComponent";
 import "./ProjectList.css";
 
-export default class ProjectList extends Component {
-    render() {
-        return (
+function ProjectList() {
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch(
+                    "http://77.243.85.19:1337/api/projects?populate=*"
+                );
+                const data = await response.json();
+                setProjects(data.data);
+                setLoading(false);
+                console.log(data.data);
+            } catch (error) {
+                console.error("Error fetching projects:", error);
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <h1>===========</h1>
             <div className="ProjectList container">
-                <ProjectComponent className="project" />
-                <ProjectComponent className="project" />
-                <ProjectComponent className="project" />
-                <ProjectComponent className="project" />
-                <ProjectComponent className="project" />
-                <ProjectComponent className="project" />
+                {loading ? (
+                    <p>Loading projects...</p>
+                ) : (
+                    projects.map((project) => (
+                        <ProjectComponent
+                            key={project.id}
+                            className="project"
+                            title={project.attributes.title}
+                            category={project.attributes.category}
+                            imgURL={
+                                "http://77.243.85.19:1337" +
+                                project.attributes.projectPhotos.data[0]
+                                    .attributes.formats.thumbnail.url
+                            }
+                        />
+                    ))
+                )}
             </div>
-        );
-    }
+        </>
+    );
 }
+
+export default ProjectList;
