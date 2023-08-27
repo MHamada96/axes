ebb66078944050f5035a36cf9f2f83e9e5b4252b
Mohamed Hamada <mohamedhamada.cs@gmail.com>
2023-08-27 03:53:49 +0300

    fixed footer width and icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
 import "./Footer.css";
-import {
-    FaBehanceSquare,
-    FaFacebookSquare,
-    FaInstagram,
-    FaLinkedin,
-    FaVimeoV,
-    FaYoutube,
-} from "react-icons/fa";
-import logo from "../../assets/logo1.png";
-import logoName from "../../assets/axes digital agency.png";
+import { FaBehanceSquare, FaFacebookSquare, FaInstagram } from "react-icons/fa";
+
 import LoGo from "../../assets/LogO.png";
 
 export default class Footer extends Component {
@@ -18,12 +10,27 @@ export default class Footer extends Component {
             <div className="Footer dark">
                 <div className="Container">
                     <div className="icons">
-                        <FaFacebookSquare className="ico" size={30} />
-                        <FaBehanceSquare className="ico" size={30} />
-                        <FaLinkedin className="ico" size={30} />
-                        <FaInstagram className="ico" size={30} />
-                        <FaYoutube className="ico" size={30} />
-                        <FaVimeoV className="ico" size={30} />
+                        <a
+                            href="https://www.facebook.com/AxesDigitalAgency/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaFacebookSquare className="ico" size={30} />
+                        </a>
+                        <a
+                            href="https://www.behance.net/axesdigital"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaBehanceSquare className="ico" size={30} />
+                        </a>
+                        <a
+                            href="https://www.instagram.com/axes_digital_agency/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaInstagram className="ico" size={30} />
+                        </a>
                     </div>
                     <div className="hr">
                         <hr />
